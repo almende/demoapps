72307753cad3a4d768ba31ee1b76a22c8543feb5
Ludo Stellingwerff <ludo@almende.org>
2014-09-26 16:10:13 +0200

    Add sending of events

--- arumEventPlayer/src/main/java/com/almende/arum/EventPusher.java
+++ arumEventPlayer/src/main/java/com/almende/arum/EventPusher.java
@@ -4,6 +4,9 @@
  */
 package com.almende.arum;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -11,6 +14,10 @@ import java.net.SocketException;
 import java.net.URI;
 import java.util.Enumeration;
 import java.util.logging.Logger;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import com.almende.eve.agent.Agent;
 import com.almende.eve.agent.AgentConfig;
@@ -29,10 +36,10 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
  * The Class EventPusher.
  */
 public class EventPusher extends Agent {
-	private static final Logger LOG = Logger.getLogger(EventPusher.class
-			.getName());
-	private static final EventPusher	SINGLETON	= new EventPusher();
-	private static final URI			monitor		= URI.create("wsclient:monitor");
+	private static final Logger			LOG				= Logger.getLogger(EventPusher.class
+																.getName());
+	private static final EventPusher	SINGLETON		= new EventPusher();
+	private static final URI			agentGenerator	= URI.create("wsclient:agentGenerator");
 
 	/**
 	 * Send events.
@@ -46,8 +53,38 @@ public class EventPusher extends Agent {
 	public void sendEvents(@Optional @Name("from") Long starttime,
 			@Optional @Name("to") Long endtime) {
 
+		/*
+		 * var event = {
+		 * time: new Date(), // timestamp of time of event
+		 * performedBy: this.workers[0], // who is the agent who should handle
+		 * the job
+		 * type: 'worker', // type of the agent above, could be clear from name,
+		 * then not needed
+		 * assignment: 'makeMachine', // title of job
+		 * operation: 'start', // start / finish / pause / resume/ endOfDay /
+		 * startOfDay
+		 * id: jobId, // unique job ID, not unique event id. Used to match
+		 * operations on jobs
+		 * prerequisites: [] // array of jobIds to watch (strings), or array of
+		 * JSON objects containing agentId and type (corresponding to
+		 * performedBy and assignments fields) or array of JSON objects only
+		 * containing type.
+		 */
+
 	}
-	
+
+	/**
+	 * Send event.
+	 *
+	 * @param event
+	 *            the event
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	private void sendEvent(ObjectNode event) throws IOException {
+		caller.call(agentGenerator, "receiveEvent", event);
+	}
+
 	private String getHostAddress() throws SocketException {
 		Enumeration<NetworkInterface> e = NetworkInterface
 				.getNetworkInterfaces();
@@ -68,7 +105,6 @@ public class EventPusher extends Agent {
 		return null;
 	}
 
-	
 	/**
 	 * Inits the Conference Cloud Agent.
 	 */
@@ -113,7 +149,7 @@ public class EventPusher extends Agent {
 
 		LOG.warning("Started Conductor at:" + host);
 	}
-	
+
 	/**
 	 * The main method.
 	 *
@@ -123,5 +159,72 @@ public class EventPusher extends Agent {
 	public static void main(final String[] args) {
 		SINGLETON.init();
 	}
-	
+
+	/**
+	 * Load events.
+	 *
+	 * @param filename
+	 *            the filename
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	@Access(AccessType.PUBLIC)
+	public void loadEvents(@Name("filename") String filename, @Optional @Name("actuallySend") Boolean actuallySend)
+			throws IOException {
+		final BufferedReader reader = new BufferedReader(new FileReader(
+				filename));
+		final DateTimeFormatter formatter = DateTimeFormat
+				.forPattern("MM/dd/yyyy HH:mm:ss");
+
+		String line = reader.readLine();
+		LOG.warning("Reading line:" + line);
+		while (line != null) {
+
+			if (line.isEmpty()) {
+				continue;
+			}
+			final ObjectNode event = JOM.createObjectNode();
+			final String[] elements = line.split(",");
+			if (!elements[0].equals("jobId")) {
+				event.put("jobId", elements[0]);
+				event.put("time", DateTime.parse(elements[1], formatter)
+						.toString());
+				event.put("performedBy", elements[2]);
+				event.put("type", elements[3]);
+				event.put("assignment", elements[4]);
+				event.put("productId", elements[5]);
+				event.put("operation", elements[6]);
+
+				if (elements.length > 7) {
+					final String[] requirements = elements[7].split(";");
+					final ArrayNode reqs = JOM.createArrayNode();
+					for (String req : requirements) {
+						req = req.replaceAll("\"", "");
+						if (req.contains("(")) {
+							final ObjectNode r = JOM.createObjectNode();
+							r.put("type",
+									req.substring(0, req.indexOf("(") - 1));
+							r.put("agentId",
+									req.substring(req.indexOf("(") + 1,
+											req.indexOf(")")));
+							reqs.add(r);
+						} else {
+							final ObjectNode r = JOM.createObjectNode();
+							r.put("type", req);
+							reqs.add(r);
+						}
+					}
+					event.set("prerequisites", reqs);
+				}
+				LOG.warning("Sending event:" + event.toString());
+				if (actuallySend != null && actuallySend) {
+					sendEvent(event);
+				}
+			}
+			line = reader.readLine();
+
+		}
+		reader.close();
+	}
+
 }
